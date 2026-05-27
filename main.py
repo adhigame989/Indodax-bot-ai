ab1129a106aa807c6b16230a9405c9b6b8ed09d3
adhigame989 <adhigame989@gmail.com>
2026-05-27 23:39:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# main.py
+
 from flask import Flask
 import ccxt
 import os
 import config
+import trader
 
 from scanner import (
     market_data,
@@ -9,13 +12,15 @@ from scanner import (
 )
 
 from trader import (
-    active_trade,
     start_trader
+)
+
+from history import (
+    get_stats
 )
 
 app = Flask(__name__)
 
-# START BACKGROUND SYSTEM
 start_scanner()
 start_trader()
 
@@ -25,92 +30,131 @@ def home():
 
     try:
 
-        # CONNECT INDODAX
         exchange = ccxt.indodax({
             'apiKey': config.API_KEY,
             'secret': config.SECRET_KEY,
             'enableRateLimit': True
         })
 
-        # GET BALANCE
         balance = exchange.fetch_balance()
 
-        idr = balance['total'].get('IDR', 0)
+        idr = balance['total'].get(
+            'IDR',
+            0
+        )
 
-        # HTML START
+        stats = get_stats()
+
         html = f"""
+
         <html>
 
         <head>
 
-            <title>INDODAX AI BOT</title>
+            <title>
+            INDODAX AI BOT
+            </title>
 
-            <meta name="viewport"
-            content="width=device-width, initial-scale=1">
+            <meta
+            name="viewport"
+            content="width=device-width,
+            initial-scale=1">
 
             <style>
 
                 body {{
+
                     background:#0f172a;
                     color:white;
                     font-family:Arial;
                     padding:15px;
                     margin:0;
+
                 }}
 
                 h1 {{
+
                     color:#38bdf8;
                     text-align:center;
+
                 }}
 
                 .card {{
+
                     background:#1e293b;
                     padding:15px;
                     border-radius:15px;
                     margin-bottom:15px;
-                    box-shadow:0 0 10px rgba(0,0,0,0.3);
+                    box-shadow:
+                    0 0 10px
+                    rgba(0,0,0,0.3);
+
                 }}
 
                 .title {{
+
                     color:#38bdf8;
                     font-size:22px;
                     font-weight:bold;
+
                 }}
 
                 .buy {{
+
                     color:#22c55e;
                     font-weight:bold;
+
                 }}
 
                 .strong-buy {{
+
                     color:#00ff99;
                     font-weight:bold;
+
                 }}
 
                 .watch {{
+
                     color:#facc15;
                     font-weight:bold;
+
                 }}
 
                 .wait {{
+
                     color:#94a3b8;
                     font-weight:bold;
+
                 }}
 
                 .profit {{
+
                     color:#22c55e;
                     font-weight:bold;
                     font-size:20px;
+
                 }}
 
                 .loss {{
+
                     color:#ef4444;
                     font-weight:bold;
                     font-size:20px;
+
                 }}
 
                 .label {{
+
                     color:#94a3b8;
+
+                }}
+
+                hr {{
+
+                    border:none;
+                    border-top:
+                    1px solid #334155;
+
                 }}
 
             </style>
@@ -119,23 +163,26 @@ def home():
 
         <body>
 
-            <h1>INDODAX AI BOT</h1>
+            <h1>
+            INDODAX AI BOT
+            </h1>
 
             <div class="card">
 
                 <div class="title">
-                    BOT STATUS: ONLINE
+                    BOT STATUS
                 </div>
 
                 <br>
 
-                <div class="label">
-                    Saldo IDR
-                </div>
+                <p>
+                    ONLINE
+                </p>
 
-                <h1>
+                <p>
+                    IDR:
                     Rp {idr:,.0f}
-                </h1>
+                </p>
 
                 <p>
 
@@ -155,14 +202,54 @@ def home():
                 </p>
 
             </div>
+
+            <div class="card">
+
+                <div class="title">
+                    BOT STATS
+                </div>
+
+                <br>
+
+                <p>
+                    Total Trades:
+                    {stats['total_trades']}
+                </p>
+
+                <p>
+                    Win:
+                    {stats['win']}
+                </p>
+
+                <p>
+                    Loss:
+                    {stats['loss']}
+                </p>
+
+                <p>
+                    Winrate:
+                    {stats['winrate']}%
+                </p>
+
+                <p>
+                    Total Profit:
+                    {stats['total_profit']}%
+                </p>
+
+            </div>
+
         """
 
-        # OPEN POSITION
-        if active_trade:
+        if trader.active_trade:
 
             profit_class = "profit"
 
-            if active_trade["profit_percent"] < 0:
+            if (
+                trader.active_trade[
+                    "profit_percent"
+                ] < 0
+            ):
+
                 profit_class = "loss"
 
             html += f"""
@@ -176,51 +263,91 @@ def home():
                 <br>
 
                 <p>
-                    <span class="label">Coin:</span>
-                    {active_trade['symbol']}
+
+                    Coin:
+
+                    {trader.active_trade['symbol']}
+
                 </p>
 
                 <p>
-                    <span class="label">Buy Price:</span>
-                    {active_trade['buy_price']}
+
+                    Buy Price:
+
+                    {
+                        trader.active_trade[
+                            'buy_price'
+                        ]
+                    }
+
                 </p>
 
                 <p>
-                    <span class="label">Current Price:</span>
-                    {active_trade['current_price']}
+
+                    Current Price:
+
+                    {
+                        trader.active_trade[
+                            'current_price'
+                        ]
+                    }
+
                 </p>
 
                 <p class="{profit_class}">
+
                     Profit:
-                    {active_trade['profit_percent']}%
+
+                    {
+                        trader.active_trade[
+                            'profit_percent'
+                        ]
+                    }%
+
                 </p>
 
                 <p>
-                    <span class="label">Take Profit:</span>
-                    {active_trade['tp_price']:.2f}
+
+                    TP:
+
+                    {
+                        trader.active_trade[
+                            'tp_price'
+                        ]
+                    }
+
                 </p>
 
                 <p>
-                    <span class="label">Stop Loss:</span>
-                    {active_trade['sl_price']:.2f}
+
+                    SL:
+
+                    {
+                        trader.active_trade[
+                            'sl_price'
+                        ]
+                    }
+
                 </p>
 
             </div>
 
             """
 
-        # MARKET SCANNER
         for coin in market_data:
 
             signal_class = "wait"
 
             if coin['signal'] == "BUY":
+
                 signal_class = "buy"
 
             elif coin['signal'] == "STRONG BUY":
+
                 signal_class = "strong-buy"
 
             elif coin['signal'] == "WATCH":
+
                 signal_class = "watch"
 
             html += f"""
@@ -228,38 +355,52 @@ def home():
             <div class="card">
 
                 <div class="title">
+
                     {coin['symbol']}
+
                 </div>
 
                 <br>
 
                 <p class="{signal_class}">
+
                     {coin['signal']}
+
                 </p>
 
                 <p>
-                    <span class="label">Score:</span>
+
+                    Score:
                     {coin['score']}/100
+
                 </p>
 
                 <p>
-                    <span class="label">RSI:</span>
+
+                    RSI:
                     {coin['rsi']}
+
                 </p>
 
                 <p>
-                    <span class="label">Price:</span>
+
+                    Price:
                     {coin['price']}
+
                 </p>
 
                 <p>
-                    <span class="label">Volume:</span>
+
+                    Volume:
                     {coin['volume']:,.0f}
+
                 </p>
 
                 <p>
-                    <span class="label">Spread:</span>
+
+                    Spread:
                     {coin['spread']:.2f}%
+
                 </p>
 
             </div>
@@ -267,6 +408,46 @@ def home():
             """
 
         html += """
+
+        <div class="card">
+
+            <div class="title">
+                TRADE HISTORY
+            </div>
+
+            <br>
+
+        """
+
+        for trade_data in stats["history"][:10]:
+
+            html += f"""
+
+            <p>
+
+                {trade_data['time']}
+
+                <br><br>
+
+                {trade_data['symbol']}
+
+                |
+
+                {trade_data['side']}
+
+                |
+
+                {trade_data['profit_percent']}%
+
+            </p>
+
+            <hr>
+
+            """
+
+        html += """
+
+        </div>
 
         </body>
         </html>
@@ -301,7 +482,12 @@ def home():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
 
     app.run(
         host="0.0.0.0",
