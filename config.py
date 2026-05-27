efe1f71726b24488976f906cc4761918794d9e12
adhigame989 <adhigame989@gmail.com>
2026-05-28 00:22:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,85 +1,37 @@
-import os
+API_KEY = "ISI_API_KEY"
 
-# =========================================
-# API INDODAX
-# =========================================
+SECRET_KEY = "ISI_SECRET_KEY"
 
-API_KEY = os.environ.get(
-    "API_KEY",
-    ""
-)
-
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    ""
-)
-
-# =========================================
-# TRADING SETTINGS
-# =========================================
-
-# modal per trade
 BASE_TRADE_AMOUNT = 100000
 
-# maksimum posisi aktif
-MAX_ACTIVE_TRADES = 8
+MAX_TRADE_AMOUNT = 500000
 
-# take profit %
+COMPOUND_PERCENT = 10
+
+MAX_ACTIVE_TRADES = 1
+
 TAKE_PROFIT = 8
 
-# stop loss %
 STOP_LOSS = 5
-
-# =========================================
-# TRAILING STOP
-# =========================================
 
 TRAILING_STOP = True
 
-# trailing gap %
 TRAILING_GAP = 2
 
-# =========================================
-# SLIPPAGE
-# =========================================
+BUY_SLIPPAGE = 0.001
 
-# buy +0.2%
-BUY_SLIPPAGE = 0.002
-
-# sell -0.2%
-SELL_SLIPPAGE = 0.002
-
-# =========================================
-# MARKET SCANNER
-# =========================================
+SELL_SLIPPAGE = 0.001
 
 TIMEFRAME = "15m"
 
 SCAN_LIMIT = 25
 
-# minimum volume market
-MIN_VOLUME = 100000000
+SCANNER_INTERVAL = 60
 
-# maksimum spread %
-MAX_SPREAD = 3
+TRADER_INTERVAL = 10
 
-# =========================================
-# FILTER
-# =========================================
-
-ENABLE_BTC_FILTER = True
+MIN_VOLUME = 50000000
 
 ENABLE_SPREAD_FILTER = True
 
-# =========================================
-# SAFETY
-# =========================================
-
-# minimum saldo IDR
-MINIMUM_IDR = 50000
-
-# delay trader loop
-TRADER_INTERVAL = 15
-
-# delay scanner update
-SCANNER_INTERVAL = 300
+MAX_SPREAD = 1.5
