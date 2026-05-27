8d0b260a903def817aa814d2c9042c9b92a8b171
adhigame989 <adhigame989@gmail.com>
2026-05-27 17:45:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask
 import ccxt
+import os
 import config
 
+from scanner import (
+    market_data,
+    start_scanner
+)
+
 app = Flask(__name__)
+
+# START BACKGROUND SCANNER
+start_scanner()
 
 @app.route("/")
 def home():
@@ -19,28 +28,85 @@ def home():
 
         idr = balance['total'].get('IDR', 0)
 
-        return f"""
+        html = f"""
         <html>
 
-        <body style="
-            background:#0f172a;
-            color:white;
-            font-family:Arial;
-            padding:20px;
-        ">
+        <head>
 
-        <h1>INDODAX AI BOT</h1>
+            <title>INDODAX AI BOT</title>
 
-        <h2>BOT ONLINE</h2>
+            <meta name="viewport"
+            content="width=device-width, initial-scale=1">
 
-        <h3>Saldo IDR:</h3>
+            <style>
 
-        <h1>Rp {idr:,.0f}</h1>
+                body {{
+                    background:#0f172a;
+                    color:white;
+                    font-family:Arial;
+                    padding:15px;
+                }}
+
+                .card {{
+                    background:#1e293b;
+                    padding:15px;
+                    border-radius:12px;
+                    margin-bottom:15px;
+                }}
+
+                h1 {{
+                    color:#38bdf8;
+                }}
+
+            </style>
+
+        </head>
+
+        <body>
+
+            <h1>INDODAX AI BOT</h1>
+
+            <div class="card">
+
+                <h2>BOT ONLINE</h2>
+
+                <h3>Saldo IDR</h3>
+
+                <h1>Rp {idr:,.0f}</h1>
+
+            </div>
+        """
+
+        for coin in market_data:
+
+            html += f"""
+
+            <div class="card">
+
+                <h2>{coin['symbol']}</h2>
+
+                <p>Signal: {coin['signal']}</p>
+
+                <p>Score: {coin['score']}/100</p>
+
+                <p>RSI: {coin['rsi']}</p>
+
+                <p>Price: {coin['price']}</p>
+
+                <p>Spread: {coin['spread']:.2f}%</p>
+
+            </div>
+
+            """
+
+        html += """
 
         </body>
-
         </html>
+
         """
+
+        return html
 
     except Exception as e:
 
@@ -48,4 +114,10 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
