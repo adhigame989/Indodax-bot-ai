1681a06456bb906d06e24667295ca91839785b62
adhigame989 <adhigame989@gmail.com>
2026-05-27 17:03:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from flask import Flask
+import ccxt
+import config
 
 app = Flask(__name__)
 
+exchange = ccxt.indodax({
+    'apiKey': config.API_KEY,
+    'secret': config.SECRET_KEY
+})
+
 @app.route("/")
 def home():
-    return """
-    <h1>INDODAX AI BOT</h1>
-    <p>Bot Running...</p>
-    """
+
+    try:
+        balance = exchange.fetch_balance()
+
+        idr = balance['total'].get('IDR', 0)
+
+        html = f"""
+        <h1>INDODAX AI BOT</h1>
+
+        <h2>Bot Status: ONLINE</h2>
+
+        <h3>Saldo IDR:</h3>
+        <p>Rp {idr:,.0f}</p>
+        """
+
+        return html
+
+    except Exception as e:
+        return f"ERROR: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
